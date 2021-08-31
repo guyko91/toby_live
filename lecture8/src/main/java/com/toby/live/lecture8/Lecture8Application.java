fb4d3b92c5719a6a407021c8b343e51880a0cf9f
guyko91 <guyko91@gmail.com>
2021-08-31 11:32:38 +0900

    8강 마무리

--- lecture8/src/main/java/com/toby/live/lecture8/Lecture8Application.java
+++ lecture8/src/main/java/com/toby/live/lecture8/Lecture8Application.java
@@ -5,12 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
+import java.io.IOException;
+import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @EnableAsync
 @SpringBootApplication
+@RestController
 public class Lecture8Application {
 
 
@@ -49,6 +57,48 @@ public class Lecture8Application {
 //                return "hello";
 //        }
 
+
+	Queue<DeferredResult<String>> results = new ConcurrentLinkedDeque<>();
+
+	@GetMapping("/dr")
+	public DeferredResult<String> dr() {
+		log.info("dr");
+		DeferredResult<String> dr = new DeferredResult<>(600000L);
+		results.add(dr);
+		return dr;
+	}
+
+	@GetMapping("/dr/count")
+	public String drCount() {
+		return String.valueOf(results.size());
+	}
+
+	@GetMapping("/dr/event")
+	public String drEvent(String msg) {
+		for(DeferredResult<String> dr : results) {
+			dr.setResult("Hello " + msg);
+			results.remove(dr);
+		}
+		return "OK";
+	}
+
+	// 스트리밍 방식으로 response
+	@GetMapping("/emitter")
+	public ResponseBodyEmitter emitter() throws InterruptedException {
+		ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+
+		Executors.newSingleThreadExecutor().submit(() -> {
+			for(int i = 0; i <= 50; i++) {
+				try {
+					emitter.send("<p>Stream " + i + "</p>");
+					Thread.sleep(100);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		return emitter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lecture8Application.class, args);
