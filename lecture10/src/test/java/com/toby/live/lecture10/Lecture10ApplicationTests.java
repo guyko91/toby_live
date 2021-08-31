f158fc58005f0a08d46fae9b88bd838feb9e3c09
guyko91 <guyko91@gmail.com>
2021-08-31 14:52:31 +0900

    lecture 9 ~ 10

---
+++ lecture10/src/test/java/com/toby/live/lecture10/Lecture10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.live.lecture10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
