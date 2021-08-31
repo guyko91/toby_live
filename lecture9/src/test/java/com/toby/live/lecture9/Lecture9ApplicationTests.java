f4abf9640aafdf36065d6824929f1b65dc2c636e
guyko91 <guyko91@gmail.com>
2021-08-31 13:33:01 +0900

    자바 비동기 방식 마무리

---
+++ lecture9/src/test/java/com/toby/live/lecture9/Lecture9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.live.lecture9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
